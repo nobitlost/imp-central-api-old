a74be2801024969e66c5d8bd651224855f0698c2
ekaterina-nbl <ekaterina.vergizova@nobitlost.com>
2017-04-25 15:27:12 +0300

    library api

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = require('./lib/ImpCentralApi');
+module.exports.Deployments = require('./lib/Deployments');
+module.exports.DeviceGroups = require('./lib/DeviceGroups');
+module.exports.Devices = require('./lib/Devices');
+module.exports.Products = require('./lib/Products');
